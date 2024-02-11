4f5354733811f1ff77a27b97ec607dadee181d4f
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2024-02-11 10:27:42 +0330

    complete header

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 import BrownLink from "@/components/reusableComponents/brownLink/BrownLink";
-import { Divider } from "@mui/material";
+import { Button, Divider } from "@mui/material";
+import Image from "next/image";
+import CustomButton from "@/components/reusableComponents/customButton/customButton";
 
 export default async function Home() {
   return (
     <div className={styles.container}>
       <section className={styles.navbar}>
-        <h3 style={{width:'13%'}}>Tiara Academy</h3>
+        <h3 style={{ width: "13%" }}>Tiara Academy</h3>
         <div className={styles.navLinks}>
           <div className={styles.navbarLeft}>
             {" "}
-    
             <Link href={"/home"}>Services</Link>
-            <Divider orientation="vertical" style={{height:'20px' }}/>
+            <Divider orientation="vertical" style={{ height: "20px" }} />
             <Link href={"/home"}>Levels</Link>
-            <Divider orientation="vertical" style={{height:'20px' }}/>
+            <Divider orientation="vertical" style={{ height: "20px" }} />
             <Link href={"/home"}>Testimonials</Link>
-            <Divider orientation="vertical" style={{height:'20px' }}/>
+            <Divider orientation="vertical" style={{ height: "20px" }} />
             <Link href={"/home"}>Enter Main Website</Link>
-          
           </div>
           <div className={styles.navbarRight}>
             <span>FAQs</span>
@@ -27,7 +27,47 @@ export default async function Home() {
           </div>
         </div>
       </section>
-      <section className={styles.header}>header</section>
+      <Divider />
+      <section className={styles.header}>
+        <div className={styles.leftHeader}>
+          <p style={{ marginBottom: "2em" }}>- Benefits</p>
+          <p style={{ fontWeight: 700, fontSize: 70 }}>
+            The Best Way
+            <br />
+            to showcase
+            <br />
+            your project.
+          </p>
+          <p style={{ marginBottom: "1em" }}>
+            Here you can put a short description about your project.
+          </p>
+          <div style={{ display: "flex", gap: "1em" }}>
+            <BrownLink title="Try for free" href="#" />
+            <Button
+              variant="outlined"
+              sx={{
+                "&.MuiButton-root": {
+                  borderRadius: "2px",
+                  color: "black",
+                  borderColor: "black",
+                },
+              }}
+            >
+              See how it works
+            </Button>
+          </div>
+        </div>
+
+        <div className={styles.rightHeader}>
+          <Image
+            src="https://images.pexels.com/photos/5676740/pexels-photo-5676740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Hero image"
+            width={400}
+            height={500}
+          />
+        </div>
+      </section>
+
       <section className={styles.servicesFirst}>servicesFirst</section>
       <section className={styles.serviceSecond}>serviceSecond</section>
       <section className={styles.serviceThird}>serviceThird</section>
--- components/reusableComponents/customButton/CustomButton.tsx
+++ components/reusableComponents/customButton/CustomButton.tsx
@@ -1,22 +1,27 @@
 import { Button } from "@mui/material";
 import React from "react";
+const defautStyles = {
+  backgroundColor: "#81403e",
+  "&:hover": { backgroundColor: "#915e5c" },
 
+}
 const CustomButton = ({
   name,
   handleClick,
   href,
+  variant='contained',
+  styles=defautStyles
 }: {
   name: string;
   handleClick?: () => void;
   href?: string;
+  variant?:'contained' | 'outlined' | 'text'
+  styles: {[key: string]: string | number | {[key: string]: string}}
 }) => {
   return (
     <Button
-      variant="contained"
-      sx={{
-        backgroundColor: "#81403e",
-        "&:hover": { backgroundColor: "#915e5c" },
-      }}
+      variant={variant}
+      sx={styles}
       onClick={handleClick}
       href={href}
     >
