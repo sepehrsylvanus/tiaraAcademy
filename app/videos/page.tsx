f37b8b9e84995d41884f00c0a4bb103b35678204
Sylvanus <sepehr.haghdoust@gmail.com>
2024-03-14 19:28:46 +0330

    videos page

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -37,25 +37,26 @@ const Videos = () => {
   return (
     <div className="flex flex-col px-4 md:pl-[4em]">
       <div>
-        <Input
-          type="text"
-          placeholder="Search name..."
-          className=" rounded-3xl mt-4"
-        />
+        <div className="flex justify-between items-center gap-4 mt-4">
+          <h1 className="h1 ">Videos</h1>
+          <Input
+            type="text"
+            placeholder="Search name..."
+            className=" rounded-3xl w-1/2"
+          />
+        </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 grid-flow-dense mt-2">
           {videoTags.map((tag) => (
-            <Badge className="w-fit bg-slate-300 text-black">{tag.name}</Badge>
+            <Badge className="w-fit bg-slate-300 text-black cursor-pointer">
+              {tag.name}
+            </Badge>
           ))}
         </div>
 
         <div className="mt-8 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {howTooStudy.map((eachCard, index) => (
-            <Card key={index}>
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
+            <Card className="p-4" key={index}>
+              <CardContent className="p-0">
                 <div className=" relative w-full h-[200px]">
                   <Image
                     src={"/article.jpg"}
@@ -73,18 +74,12 @@ const Videos = () => {
                       transform: "translate(-50%, -50%)",
                     }}
                   />
+                  <span className=" absolute  bottom-0 right-[.5em] text-white">
+                    2:45
+                  </span>
                 </div>
-                <h4 className="font-bold my-2">{eachCard.title}</h4>
-                <p className="text-sm">{eachCard.desc}</p>
+                <h4 className="font-bold mt-2">{eachCard.title}</h4>
               </CardContent>
-              <CardFooter>
-                <Link
-                  className=" text-blue-400 transition underline md:no-underline	md:transition md:hover:underline"
-                  href={"#"}
-                >
-                  Read more
-                </Link>
-              </CardFooter>
             </Card>
           ))}
         </div>
