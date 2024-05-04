5cda2cfeba3d13031a0bc3ba2179a18cc5cd3dfc
Sylvanus <sepehr.haghdoust@gmail.com>
2024-05-04 06:42:27 +0330

    protect sign out route

--- app/api/signout/route.ts
+++ app/api/signout/route.ts
@@ -1,7 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async () => {
-  const res = NextResponse.json({ message: "You logged out" });
-  res.cookies.delete("token");
-  return res;
+export const GET = async (req: NextRequest) => {
+  if (req.headers.get("apiKey")) {
+    const res = NextResponse.json({ message: "You logged out" });
+    res.cookies.delete("token");
+    return res;
+  } else {
+    return NextResponse.json({ message: "Access Denied" }, { status: 401 });
+  }
 };
