970562cd9c08655c083c43cda9f6f416349c9759
Sylvanus <sepehr.haghdoust@gmail.com>
2024-09-17 11:43:20 +0300

    add home logo to pages

--- app/hub/layout.tsx
+++ app/hub/layout.tsx
@@ -2,6 +2,8 @@ import { getToken } from "@/actions/actions";
 import CustomHamburger from "@/components/hamburger/CustomHamburger";
 import Sidebar from "@/components/sidebar/Sidebar";
 import { getLocale } from "next-intl/server";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
@@ -16,7 +18,9 @@ const layout = async ({ children }: { children: React.ReactNode }) => {
       <div className="ml-auto z-10 fixed top-0 right-0 md:hidden bg-white  rounded-md m-2">
         <CustomHamburger navbar={false} sidebar={true} />
       </div>
-
+      <Link href={"/"} className="absolute top-2 left-2">
+        <Image src={"/logo.svg"} alt="Logo" width={60} height={60} />
+      </Link>
       <div
         className={` ${locale === "fa" ? "lg:pr-[3em]" : "lg:pl-[2.5em]"} pt-4`}
       >
