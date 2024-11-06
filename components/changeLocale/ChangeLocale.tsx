6d668d09257918ceb7ffb204ae38a8f0742d5d31
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2024-11-07 01:38:51 +0300

    change language style

--- components/changeLocale/ChangeLocale.tsx
+++ components/changeLocale/ChangeLocale.tsx
@@ -3,26 +3,52 @@ import React from "react";
 import { Button } from "../ui/button";
 import { setUserLocale } from "@/services/locale";
 import { useLocale, useTranslations } from "next-intl";
+import Image from "next/image";
 
 const ChangeLocale = () => {
   const language = useLocale();
-  console.log(language);
+
   const t = useTranslations("Navbar");
-  console.log(t("language"));
-  return (
-    <Button
-      onClick={() => {
-        if (language === "fa") {
-          setUserLocale("en");
-        } else {
-          setUserLocale("fa");
-        }
-      }}
-      className="hover:bg-extraText transition"
-    >
-      {t("language")}
-    </Button>
-  );
+
+  if (language === "fa") {
+    return (
+      <div className="relative w-[30px] h-[20px] hover:shadow-2xl cursor-pointer">
+        <Image
+          src={"/landingPageSections/iranFlag.jpg"}
+          fill
+          alt="iran flag"
+          className="hover:scale-150 transition-all absolute "
+          onClick={() => {
+            if (language === "fa") {
+              setUserLocale("en");
+            } else {
+              setUserLocale("fa");
+            }
+          }}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="relative w-[30px] h-[20px] hover:shadow-2xl cursor-pointer">
+        <Image
+          src={"/landingPageSections/unitedStateFlag.png"}
+          width={40}
+          height={40}
+          alt="united state flag"
+          className="hover:scale-150 transition-all absolute "
+          onClick={() => {
+            if (language === "fa") {
+              setUserLocale("en");
+            } else {
+              setUserLocale("fa");
+            }
+          }}
+        />
+      </div>
+    );
+  }
+  <Button className="hover:bg-extraText transition"></Button>;
 };
 
 export default ChangeLocale;
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,38 +1,37 @@
-import { Divider } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import ChangeLocale from "../changeLocale/ChangeLocale";
-import ClerkAvatar from "../reusableComponents/ClerkAvatar";
+
 import CustomHamburger from "../hamburger/CustomHamburger";
 import { getMessages } from "next-intl/server";
 import styles from "@/app/page.module.css";
-import { cookies } from "next/headers";
+
 import NavRight from "../navRight/NavRight";
 import { getToken } from "@/actions/actions";
 const Navbar = async () => {
   const message = (await getMessages()) as any;
   const token = await getToken();
-  
+
   return (
     <nav
       className={`${styles.navbar} bg-lightPrime text-extraText flex items-center `}
     >
-      <Link href={"/"}>
-        <Image
-          src={"/logo.png"}
-          alt={message.logoAlt}
-          width={85}
-          height={85}
-          className="scale-125 mr-8"
-        />
-      </Link>
-      <div
-        className={`${styles.navbarLeft} bg-lightPrime text-lightText items-center`}
-      >
-    
-     
-        <ChangeLocale />
+      <div className="flex gap-4 items-center">
+        <Link href={"/"}>
+          <Image
+            src={"/logo.png"}
+            alt={message.logoAlt}
+            width={85}
+            height={85}
+            className="scale-125 mr-8"
+          />
+        </Link>
+        <div
+          className={`${styles.navbarLeft} bg-lightPrime text-lightText items-center`}
+        >
+          <ChangeLocale />
+        </div>
       </div>
       <NavRight token={token?.value!} />
       <CustomHamburger navbar={true} sidebar={false} />
