ad276f73844cca44cb63b288e004d491e6029fe3
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2025-02-27 13:46:00 +0300

    fix typo for token secret

--- app/api/(auth)/signin/route.ts
+++ app/api/(auth)/signin/route.ts
@@ -4,65 +4,69 @@ import prisma from "@/utils/db";
 import { getMessages } from "next-intl/server";
 const { sign } = require("jsonwebtoken");
 
-type FormDataProps = {
+type formDataProps = {
   email: string;
   password: string;
   userAgent: string;
 };
 
-export const POST = async (req: NextRequest) => {
-  const formData: FormDataProps = await req.json();
+export const POST = async (req: NextRequest, res: NextResponse) => {
+  const formData: formDataProps = await req.json();
   const messages = (await getMessages()) as any;
   const signInT = messages.SignIn;
-
   const user = await prisma.user.findUnique({
     where: {
       email: formData.email,
     },
-    select: {
-      password: true,
-      agents: true,
-    },
   });
+  const agentAlreadyExists = user?.agents.includes(formData.userAgent);
+  if (agentAlreadyExists) {
+    const token = sign(
+      {
+        data: formData.email,
+      },
+      process.env.NEXT_PUBLIC_TOKEN_SERCRET,
+      {
+        expiresIn: "30d",
+      }
+    );
+    if (req.headers.get("apiKey")) {
+      const response = NextResponse.json({
+        message: signInT.success,
+      });
+
+      response.cookies.set("token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      });
+
+      return response;
+    } else {
+      return NextResponse.json({ message: "Access denied" }, { status: 401 });
+    }
+  }
 
   if (!user) {
     return NextResponse.json({ error: signInT.notFound }, { status: 404 });
   }
 
+  if (user.agents.length >= 2) {
+    return NextResponse.json({ error: signInT.tooManyDevice }, { status: 403 });
+  }
+
   const isPassValid = bcrypt.compareSync(formData.password, user.password);
+
   if (!isPassValid) {
     return NextResponse.json(
       { error: signInT.wrongCredentials },
       { status: 403 }
     );
   }
-
-  // Check if the current device is already registered
-  if (user.agents.includes(formData.userAgent)) {
-    const token = sign(
-      { data: formData.email },
-      process.env.NEXT_PUBLIC_TOKEN_SECRET!,
-      { expiresIn: "30d" }
-    );
-
-    const response = NextResponse.json({ message: signInT.success });
-    response.cookies.set("token", token, {
-      httpOnly: true,
-      expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
-
-    return response;
-  }
-
-  // If the user has more than 2 devices registered, deny the login
-  if (user.agents.length >= 2) {
-    return NextResponse.json({ error: signInT.tooManyDevice }, { status: 403 });
-  }
-
-  // Otherwise, add the new device and update the user record
   await prisma.user.update({
-    where: { email: formData.email },
+    where: {
+      email: formData.email,
+    },
     data: {
       agents: {
         push: formData.userAgent,
@@ -71,17 +75,27 @@ export const POST = async (req: NextRequest) => {
   });
 
   const token = sign(
-    { data: formData.email },
-    process.env.NEXT_PUBLIC_TOKEN_SECRET!,
-    { expiresIn: "30d" }
+    {
+      data: formData.email,
+    },
+    process.env.NEXT_PUBLIC_TOKEN_SERCRET,
+    {
+      expiresIn: "30d",
+    }
   );
+  if (req.headers.get("apiKey")) {
+    const response = NextResponse.json({
+      message: signInT.success,
+    });
 
-  const response = NextResponse.json({ message: signInT.success });
-  response.cookies.set("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-  });
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
 
-  return response;
+    return response;
+  } else {
+    return NextResponse.json({ message: "Access denied" }, { status: 401 });
+  }
 };
